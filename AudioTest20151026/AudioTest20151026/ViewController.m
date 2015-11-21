9a7f71499708238e8f2b882bde16abf60aa0f931
Antony.Wong <aeq2005@163.com>
2015-11-21 14:16:17 +0800

    左右声道切换的实现

--- AudioTest20151026/AudioTest20151026/ViewController.m
+++ AudioTest20151026/AudioTest20151026/ViewController.m
@@ -26,6 +26,12 @@
 
 @property (nonatomic, readonly) BOOL marimbaPlayerIsPlaying;
 
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
+@property (weak, nonatomic) IBOutlet UIButton *leftButton;
+@property (weak, nonatomic) IBOutlet UIButton *rightButton;
+
+@property (strong, nonatomic) UIAlertView *nonSelectedSoundtrackAlertView;
+
 @end
 
 @implementation ViewController
@@ -41,7 +47,7 @@
     
     // 创建播放的buffer
     NSError *error;
-    NSURL *marimbaLoopURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"marimbaLoop" ofType:@"caf"]];
+    NSURL *marimbaLoopURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"leftRightTest" ofType:@"mp3"]];
     AVAudioFile *marimbaLoopFile = [[AVAudioFile alloc] initForReading:marimbaLoopURL error:&error];
     _marimbaLoopBuffer = [[AVAudioPCMBuffer alloc] initWithPCMFormat:[marimbaLoopFile processingFormat] frameCapacity:(AVAudioFrameCount)[marimbaLoopFile length]];
     NSAssert([marimbaLoopFile readIntoBuffer:_marimbaLoopBuffer error:&error], @"couldn't read marimbaLoopFile into buffer, %@", [error localizedDescription]);
@@ -86,7 +92,19 @@
 }
 
 - (IBAction)togglePlayMarimba:(UIButton *)sender {
+    if (!_leftButton.selected && !_rightButton.selected) {
+        if (!_nonSelectedSoundtrackAlertView) {
+            _nonSelectedSoundtrackAlertView = [[UIAlertView alloc] initWithTitle:@"NOTE" message:@"Please select Soundtrack First." delegate:sender cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        }
+        [_nonSelectedSoundtrackAlertView show];
+        return;
+    }
+    
+    sender.selected = !sender.selected;
+
+//    _marimbaPlayer.pan = 1;
     if (!self.marimbaPlayerIsPlaying) {
+        
         [self startEngine];
         
         // 播放文件(AVAudioFile)，只能播放一次，
@@ -113,16 +131,62 @@
         // 1、(Append new buffer)两个options参数设置为nil，两个buffer默认按顺序前后播放
         // 2、(Interrupt with new buffer)第二个bufferoptions参数设置为AVAudioPlayerNodeBufferInterrupts，会打断前面播放的buffer，直接播放新buffer
         // ( Interrupt looping buffer after current loop finishes)AVAudioPlayerNodeBufferInterruptsAtLoop:等前面循环的buffer完整播完后，再打断，播放新buffer(测试过，好像不起作用)
-        [_marimbaPlayer scheduleBuffer:_marimbaLoopBuffer atTime:nil options:nil completionHandler:nil];
-        [_marimbaPlayer scheduleBuffer:_drumLoopBuffer atTime:nil options:nil completionHandler:nil];
+        [_marimbaPlayer scheduleBuffer:_marimbaLoopBuffer atTime:nil options:AVAudioPlayerNodeBufferLoops completionHandler:nil];
+//        [_marimbaPlayer scheduleBuffer:_drumLoopBuffer atTime:nil options:nil completionHandler:nil];
         
         
-        [_marimbaPlayer scheduleBuffer:_thirdBuffer atTime:nil options:AVAudioPlayerNodeBufferLoops completionHandler:nil];
+//        [_marimbaPlayer scheduleBuffer:_thirdBuffer atTime:nil options:AVAudioPlayerNodeBufferLoops completionHandler:nil];
 
         [_marimbaPlayer play];
         
     } else {
         [_marimbaPlayer stop];
+        _leftButton.selected = NO;
+        _rightButton.selected = NO;
+    }
+}
+
+#pragma mark - 左右声道切换
+// -1为左声道;1为右声道
+- (IBAction)leftButtonAction:(UIButton *)sender {
+    sender.selected = !sender.selected;
+    
+    if (sender.selected) {
+        // 左声道选择状态:1、如果右声道有选择，就设置立体声;2、如果右声道没有选择，就设置左声道
+        if (_rightButton.selected) {
+            _marimbaPlayer.pan = 0;
+        } else {
+            _marimbaPlayer.pan = -1;
+        }
+    } else {
+        // 左声道非选择状态:1、如果右声道有选择，就设置右声道;2、如果右声道没有选择，关闭声音
+        if (_rightButton.selected) {
+            _marimbaPlayer.pan = 1;
+        } else {
+            // 停止播放
+            [_marimbaPlayer stop];
+            _playButton.selected = NO;
+        }
+    }
+}
+
+- (IBAction)rightButtonAction:(UIButton *)sender {
+    sender.selected = !sender.selected;
+    
+    if (sender.selected) {
+        if (_leftButton.selected) {
+            _marimbaPlayer.pan = 0;
+        } else {
+            _marimbaPlayer.pan = 1;
+        }
+    } else {
+        if (_leftButton.selected) {
+            _marimbaPlayer.pan = -1;
+        } else {
+            // 停止播放
+            [_marimbaPlayer stop];
+            _playButton.selected = NO;
+        }
     }
 }
 
